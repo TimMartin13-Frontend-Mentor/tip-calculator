bb450c4049cec38fadf4a3158d88c71d9f3a4376
Martin <tim.d.martin@sogeti.com>
2022-06-02 09:14:47 -0400

    Added initial scss files and google font

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// const requestUrl = 'https://api.adviceslip.com/advice';
+
+// const card = document.querySelector(".card");
+// const adviceNumber = document.querySelector(".advice-number");
+// const adviceText = document.querySelector(".advice-text");
+// const adviceBtn = document.querySelector(".advice-btn");
+
+
+// const getAdvice = () => {
+//   fetch(requestUrl)
+//     .then(response => response.json())
+//     .then(data => 
+//       {
+//         adviceNumber.innerHTML = data.slip.id;
+//         adviceText.innerHTML = data.slip.advice;
+//       });
+// }
+
+// adviceBtn.addEventListener('click', getAdvice);
+  
