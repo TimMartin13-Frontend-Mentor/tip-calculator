82e6459a9b86917fc407270a7c6f4ffba07d96f0
Martin <tim.d.martin@sogeti.com>
2022-06-06 17:22:56 -0400

    Javascript logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,130 @@
-const billPrice = document.querySelector("#price");
-const customPercent = document.querySelector("#custom");
-const numOfPeople = document.querySelector("#num-people");
-const resetBtn = document.querySelector(".reset");
-const taxAmount = document.querySelector(".tax");
-let totalAmount = document.querySelector(".total");
+const billPrice = document.querySelector('#price');
+const peopleNumber = document.querySelector('#num-people');
+const customTip = document.querySelector('#custom');
+const tipPerPerson = document.querySelector('.tip-amount');
+const totalPerPerson = document.querySelector('.total-amount');
+const tips = document.querySelectorAll('.tip-button');
+const errorText = document.querySelector('.error-text');
+const resetBtn = document.querySelector('.reset');
 
-const calculatePerPerson = () => {
-  
-  numOfPeople.value = 1;
-  totalAmount.textContent = billPrice.value / numOfPeople.value;
+let price, people, tipValue;
 
+const getPrice = () => {
+  price = parseFloat(billPrice.value);
+  localStorage.setItem('price', price);
+  calculateTip();
 }
 
-billPrice.addEventListener('keyup', calculatePerPerson);
-numOfPeople.addEventListener('keyup', calculatePerPerson);
+const getPeople = () => {
+  people = parseInt(peopleNumber.value);
+  localStorage.setItem('people', people);
+  calculateTip();
+}
 
+const getCustomTip = () => {
+  tipValue = parseFloat(customTip.value / 100);
+  localStorage.setItem('customTip', customTip.value);
+  localStorage.setItem('TipPercent', 'null');
+  tips.forEach(function(val){
+    val.classList.remove('selected');
+  });
+  calculateTip();
+}
+
+const resetValues = () => {
+  //  Reset HTML
+  billPrice.value = "";
+  peopleNumber.value = "";
+  customTip.value = "";
+  tipPerPerson.innerHTML = `$0.00`;
+  totalPerPerson.innerHTML = `$0.00`;
   
+  //  Reset coded values
+  price = 0;
+  people = 1;
+  tipValue = 0;
+  
+  //  Reset stored values
+  localStorage.setItem('price', price);
+  localStorage.setItem('people', people);
+  localStorage.setItem('customTip', customTip.value);
+  localStorage.setItem('TipPercent', tipValue);
+  
+  //  Unselect buttons
+  tips.forEach(function(val){
+    val.classList.remove('selected');
+  });
+}
+
+const handleClick = (e) => {
+  tips.forEach(function(val){
+    val.classList.remove('selected');
+    if (e.target.innerHTML == val.innerHTML) {
+      val.classList.add('selected');
+      // console.log("Target: ", e.target.innerHTML, val);
+      tipValue = parseFloat(val.innerHTML) / 100;
+      localStorage.setItem('TipPercent', val.innerHTML);
+      customTip.value = "";
+      localStorage.setItem('customTip', customTip.value);
+    }
+  });
+  calculateTip();
+}
+
+billPrice.addEventListener('input', getPrice);
+peopleNumber.addEventListener('input', getPeople);
+customTip.addEventListener('input', getCustomTip);
+resetBtn.addEventListener('click', resetValues);
+tips.forEach(function(val) {
+  val.addEventListener('click', handleClick);
+});
+
+const calculateTip = () => {
+  if (people >= 1) {
+    errorText.classList.add('hide');
+    let tipAmount = (price * tipValue) / people;
+    let totalAmount = (price / people) + tipAmount;
+    tipPerPerson.innerHTML = `$${ tipAmount.toFixed(2) }`;
+    totalPerPerson.innerHTML = `$${ totalAmount.toFixed(2) }`;
+  }
+  else {
+    // Display error if less than 1 person
+    errorText.classList.remove('hide');
+  }
+}
+
+const initializeInputs = () => {
+  //  Get info from local storage
+  const storagePrice = localStorage.getItem('price');
+  const storagePeople = localStorage.getItem('people');
+  const storageCustomTip = localStorage.getItem('customTip');
+  const storageButtonTip = localStorage.getItem('TipPercent');
+
+  // If there were values in localStorage, use them
+  // otherwise initialize values to default
+  billPrice.value =  storagePrice >= 0 ? storagePrice : 0;
+  peopleNumber.value =  storagePeople >= 1 ? storagePeople : 1;
+  customTip.value =  storageCustomTip >= 0 ? storageCustomTip : 0;
+  if (customTip.value !== "") {
+    // Custom value stored
+    tipValue = customTip.value / 100;
+  }
+  else if (storageButtonTip != 0) {
+    // Button stored
+    tips.forEach(function(val){
+      if (val.innerHTML == storageButtonTip) {
+        val.classList.add('selected');
+        tipValue = parseFloat(val.innerHTML) / 100;
+      }
+    });
+  }
+  else {
+    // Default
+    tipValue = 0;
+  }
+
+  price = billPrice.value;
+  people = peopleNumber.value;
+
+  calculateTip();
+}
